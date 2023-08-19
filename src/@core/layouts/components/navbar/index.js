d4f43fa7f5d2b3734fe44db4da8868db95fe21de
Bello Kader Adetoudji <54768908+BELLO8@users.noreply.github.com>
2023-08-19 11:12:14 +0000

    Update index.js

--- src/@core/layouts/components/navbar/index.js
+++ src/@core/layouts/components/navbar/index.js
@@ -1,4 +1,4 @@
-// ** React Imports
+/* eslint-disable */
 import { Fragment } from "react"
 
 // ** Custom Components
@@ -11,9 +11,6 @@ const ThemeNavbar = (props) => {
 
   return (
     <Fragment>
-       <div className='bookmark-wrapper d-flex align-items-center'>
-        <NavbarBookmarks setMenuVisibility={setMenuVisibility} />
-      </div>
       <NavbarUser skin={skin} setSkin={setSkin} />
     </Fragment>
   )
